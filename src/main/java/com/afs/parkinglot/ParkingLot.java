5a5997a9797139290101c8cef9bdbb3680526619
qiulu2 <luca.qiu@oocl.com>
2025-09-08 17:31:22 +0800

    feat: case ~8

--- src/main/java/com/afs/parkinglot/ParkingLot.java
+++ src/main/java/com/afs/parkinglot/ParkingLot.java
@@ -15,6 +15,7 @@ public class ParkingLot {
 
     public Ticket parking(Car car) {
         if (capacity <= 0) {
+            System.out.println("No available position");
             return null;
         }
         capacity--;
@@ -26,11 +27,13 @@ public class ParkingLot {
     public Car fetchCar(Ticket ticket) {
         for (Ticket t : tickets) {
             if (t.getTicketNumber().equals(ticket.getTicketNumber())) {
+                Car car = t.getCar();
                 tickets.remove(t);
                 capacity++;
-                return t.getCar();
+                return car;
             }
         }
+        System.out.println("Unrecognized parking ticket");
         return null;
     }
 }
--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -1,11 +1,19 @@
 package com.afs.parkinglot;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.Console;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class ParkingLotTest {
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStream));
+    }
 
     // Case 1 - Given a parking lot, and a car, When park the car, Then return a parking ticket.
     @Test
@@ -31,25 +39,79 @@ public class ParkingLotTest {
         Assertions.assertEquals(car, fetchedCar);
     }
 
-//    // Case 3 - Given a parking lot with two parked cars, and two parking tickets, When fetch the car twice, Then return the right car with each ticket
-//    @Test
-//    public void should_return_the_right_car_with_each_ticket_when_given_a_parking_lot_with_two_parked_cars_and_two_parking_tickets() {
-//        Car car1 = new Car(110);
-//        Car car2 = new Car(120);
-//
-//        ParkingLot parkingLot = new ParkingLot(10);
-//
-//        Ticket ticket1 = parkingLot.parking(car1);
-//        Ticket ticket2 = parkingLot.parking(car2);
-//
-//        Car fetchedCar1 = parkingLot.fetchCar(ticket1);
-//        Car fetchedCar2 = parkingLot.fetchCar(ticket2);
-//
-//        Assertions.assertEquals(car1, fetchedCar1);
-//        Assertions.assertEquals(car2, fetchedCar2);
-//    }
+    //    // Case 3 - Given a parking lot with two parked cars, and two parking tickets, When fetch the car twice, Then return the right car with each ticket
+    @Test
+    public void should_return_the_right_car_with_each_ticket_when_given_a_parking_lot_with_two_parked_cars_and_two_parking_tickets() {
+        Car car1 = new Car(110);
+        Car car2 = new Car(120);
 
-    // Case 4 - Given a parking lot, and a wrong parking ticket, When fetch the car, Then return nothing.
-    // Case 5 - Given a parking lot, and a used parking ticket, When fetch the car, Then return nothing.
-    // Case 6 - Given a parking lot without any position, and a car, When park the car, Then return nothing
+        ParkingLot parkingLot = new ParkingLot(10);
+
+        Ticket ticket1 = parkingLot.parking(car1);
+        Ticket ticket2 = parkingLot.parking(car2);
+
+        Car fetchedCar1 = parkingLot.fetchCar(ticket1);
+        Car fetchedCar2 = parkingLot.fetchCar(ticket2);
+
+        Assertions.assertEquals(car1, fetchedCar1);
+        Assertions.assertEquals(car2, fetchedCar2);
+    }
+
+    // Case 4 - Given a parking lot, and a wrong parking ticket, When fetch the car, Then return null.
+    @Test
+    public void should_return_null_when_given_a_parking_lot_and_a_wrong_parkingTicket() {
+        Car car1 = new Car(110);
+
+        ParkingLot parkingLot = new ParkingLot(10);
+        Ticket ticket1 = parkingLot.parking(car1);
+        Ticket wrongTicket = new Ticket(new Car(120));
+
+        Assertions.assertNull(parkingLot.fetchCar(wrongTicket));
+    }
+
+    // Case 5 - Given a parking lot, and a used parking ticket, When fetch the car, Then return null.
+    @Test
+    public void should_return_null_when_given_a_parking_lot_and_a_used_parkingTicket() {
+        Car car1 = new Car(110);
+
+        ParkingLot parkingLot = new ParkingLot(10);
+        Ticket ticket1 = parkingLot.parking(car1);
+        Car car =  parkingLot.fetchCar(ticket1);
+
+        Assertions.assertNull(parkingLot.fetchCar(ticket1));
+    }
+    // Case 6 - Given a parking lot without any position, and a car, When park the car, Then return null
+    @Test
+    public void should_return_null_when_given_a_parking_lot_without_any_position_and_a_car() {
+        Car car1 = new Car(110);
+
+        ParkingLot parkingLot = new ParkingLot(0);
+        Ticket ticket1 = parkingLot.parking(car1);
+
+        Assertions.assertNull(ticket1);
+    }
+
+    // case 7 - Given a parking lot, and a wrong parking ticket, When fetch the car, Then return error msg "Unrecognized parking ticket"
+    @Test
+    public void should_return_msg_unrecognized_when_given_a_parking_lot_and_a_wrong_parking_ticket(){
+        Car car1 = new Car(110);
+        ParkingLot parkingLot = new ParkingLot(10);
+        Ticket ticket1 = parkingLot.parking(car1);
+        Ticket wrongTicket = new Ticket(new Car(120));
+        Car fetchedCar1 = parkingLot.fetchCar(wrongTicket);
+
+
+        Assertions.assertEquals("Unrecognized parking ticket", outputStream.toString().trim());
+
+    }
+
+    // case 8 - Given a parking lot without any position, and a car, When park the car, Then return error msg "No available position"
+    @Test
+    public void should_return_no_available_msg_when_given_a_parking_lot_without_any_position_and_a_car(){
+        Car car1 = new Car(110);
+        ParkingLot parkingLot = new ParkingLot(0);
+        Ticket ticket1 = parkingLot.parking(car1);
+
+        Assertions.assertEquals("No available position",outputStream.toString().trim());
+    }
 }
