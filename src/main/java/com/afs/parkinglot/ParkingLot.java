b19612ecc2ff6d5c4d70a4ebf089e6652c7318aa
qiulu2 <luca.qiu@oocl.com>
2025-09-08 16:03:19 +0800

    feat: test case 1

--- src/main/java/com/afs/parkinglot/ParkingLot.java
+++ src/main/java/com/afs/parkinglot/ParkingLot.java
@@ -1,4 +1,18 @@
 package com.afs.parkinglot;
 
 public class ParkingLot {
+
+
+
+    public ParkingLot(int i) {
+
+    }
+
+    private
+
+
+
+    public Object parking(Car car) {
+        return null;
+    }
 }
--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -1,4 +1,23 @@
 package com.afs.parkinglot;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ParkingLotTest {
+
+    // Case 1 - Given a parking lot, and a car, When park the car, Then return a parking ticket.
+    public void should_return_a_parkingTicket_when_given_a_parking_lot_and_car() {
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot(10);
+        Ticket ticket = parkingLot.parking(car);
+
+
+        Assertions.assertNotNull(ticket);
+    }
+
+    // Case 2 - Given a parking lot with a parked car, and a parking ticket, When fetch the car, Then return the parked car.
+    // Case 3 - Given a parking lot with two parked cars, and two parking tickets, When fetch the car twice, Then return the right car with each ticket
+    // Case 4 - Given a parking lot, and a wrong parking ticket, When fetch the car, Then return nothing.
+    // Case 5 - Given a parking lot, and a used parking ticket, When fetch the car, Then return nothing.
+    // Case 6 - Given a parking lot without any position, and a car, When park the car, Then return nothing
 }
